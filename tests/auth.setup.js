fb2e3faaa2ab3a434c26581fa101ad5dce1d0534
ViktoriyaED <qaviktoriaed@gmail.com>
2024-12-09 08:13:53 -0500

    Agg Cookie Authorization

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -1,28 +1,19 @@
-import { test as setup, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 import path from 'path';
+import {test as setup} from '../fixtures.js';
 import {CREDENTIALS} from "../testData";
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-setup('authenticate', async ({ page }) => {
+setup('authenticate', async ({ page, loginPage, homePage }) => {
 
-    // await page.goto('/');
-    // await loginPage.clickCloseCookieBannerButton();
-    // await loginPage.fillEmailInputField(CREDENTIALS.email);
-    // await loginPage.clickContinueButton();
-    // await loginPage.fillPasswordInputField(CREDENTIALS.password);
-    // await loginPage.clickLogInButton();
-    // await homePage.h1Header.waitFor();
-
-    await page.goto('https://app.asana.com/-/login');
-    await page.getByRole('alertdialog').getByRole('button', {name: 'Close'}).waitFor();
-    await page.getByRole('alertdialog').getByRole('button', {name: 'Close'}).click();
-    await page.locator('[type="email"]').fill(CREDENTIALS.email);
-    await page.getByRole('button', {name: 'Continue', exact: true}).click();
-    await page.locator('[type="password"]').fill(CREDENTIALS.password);
-    await page.getByRole('button', {name: 'Log in'}).click();
-    await page.getByRole('heading', {name: 'Home'}).waitFor();
-
+    await page.goto('/');
+    await loginPage.clickCloseCookieBannerButton();
+    await loginPage.fillEmailInputField(CREDENTIALS.email);
+    await loginPage.clickContinueButton();
+    await loginPage.fillPasswordInputField(CREDENTIALS.password);
+    await loginPage.clickLogInButton();
+    await homePage.h1Header.waitFor();
     await expect(page).toHaveTitle('Home - Asana');
 
     await page.context().storageState({ path: authFile });
